63eb2a9c4b89d35c157a08090c0673df9c805998
Arush Bansal <arush4bansal@gmail.com>
2024-05-09 03:51:00 +0530

    minor fix

--- app/(dashboard)/(routes)/helpcenter/page.tsx
+++ app/(dashboard)/(routes)/helpcenter/page.tsx
@@ -1,7 +1,6 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import HelpCenter from "./_components/website-embed"
-import ChatWidget from "./_components/chat-widget"
 
 export default async function Dashboard() {
   const { userId } = auth();
