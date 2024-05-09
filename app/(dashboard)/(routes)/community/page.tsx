f59a9a02332ca880cdc71983c5b1d85e2c9e58b8
Arush Bansal <arush4bansal@gmail.com>
2024-05-09 14:06:41 +0530

    arena

--- app/(dashboard)/(routes)/community/page.tsx
+++ app/(dashboard)/(routes)/community/page.tsx
@@ -1,5 +1,27 @@
+// import { auth } from "@clerk/nextjs";
+// import { redirect } from "next/navigation";
+
+// export default async function Dashboard() {
+//   const { userId } = auth();
+
+//   if (!userId) {
+//     return redirect("/");
+//   }
+
+//   return (
+//     <div className="h-full">
+//     </div>
+//   );
+// }
+
+
+
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+
+import { getDashboardCourses } from "@/actions/get-dashboard-courses";
+
+import { ArenaEmbed } from "./_components/arena-embed";
 
 export default async function Dashboard() {
   const { userId } = auth();
@@ -8,8 +30,16 @@ export default async function Dashboard() {
     return redirect("/");
   }
 
+  // const { completedCourses, coursesInProgress } = await getDashboardCourses(
+  //   userId
+  // );
+
   return (
     <div className="h-full">
+      <iframe
+        src="https://go.arena.im/embed/chat/unipace/jJwW8C1"
+        style={{border : 0, borderRadius : "4px", width : "100%", height : "100%"}}
+      ></iframe>
     </div>
   );
 }
